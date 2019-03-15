9bf66ec2e51448cb9513e4a10ea13a9d3f288984
Unknown <skrzynska.konstancja@gmail.com>
2019-03-15 13:45:55 +0100

    flying astronaut

--- src/js/js.js
+++ src/js/js.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
     $('.menu-button').on('click', function(){
         $(':nth-child(3)',this).toggleClass('open');
     })
+    $(window).on('scroll',getPotencial)
 })
+
+function getPotencial(){
+    let step = window.pageYOffset*0.3<window.innerHeight-100?window.pageYOffset*0.3:window.innerHeight-100;    
+    return $('.astronaut-box').css('transform','translateY(-'+ step + 'px)');
+}
