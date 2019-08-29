cb05d7f9fc7b642eb0dc3531e1622dc397426983
Unknown <skrzynska.konstancja@gmail.com>
2019-08-29 14:38:31 +0200

    new ideas

--- src/js/js.js
+++ src/js/js.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
     $('.menu-button').on('click', function(){
         $(':nth-child(3)',this).toggleClass('open');
-    })
+    });
     $('.nav-link').on('click',function(){
         $('.navbar-collapse').collapse('hide');
         $('.menu-button').children(2).removeClass('open');
-    })
+    });
     createCanvas();
     setInterval(shine,1000);
     $(window).on('scroll',getPotencial);
     $(window).on('resize', getPotencial);
     $(window).on('resize', createCanvas);
-})
+});
 
 function getPotencial(){
-    let h=window.innerHeight;
-    let step = window.pageYOffset*0.3<h-100 ? window.pageYOffset*0.3: h-100; 
-    if(step==h-100) $('.astronaut-text').removeClass('d-none');   
+    let h = $('.wrapper').height();
+    let step = $(window).scrollTop()*0.3<h ? $(window).scrollTop()*0.3 : h; 
+    if(step==h) $('.astronaut-text').removeClass('d-none');   
     return $('.astronaut-box').css('transform','translateY(-'+ step + 'px)');
 }
 
 const canvas = $('<canvas></canvas>').addClass('canvas-stars');
-canvas.appendTo('.home');
+$('.home').append(canvas);
 const ctx = canvas[0].getContext("2d");
 function createCanvas(){
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     canvas[0].height=canvas[0].clientHeight;
     canvas[0].width=canvas[0].clientWidth;
     for(let i=0; i<120; i++){
-        new Circle()       
+        new Circle();       
     }
 }
 function getBackgroundColor() {
@@ -52,7 +52,7 @@ function shine(){
             ctx.fillStyle=star[3];
             ctx.arc(star[0], star[1], star[2], 0, 2 * Math.PI);
             ctx.fill();
-        },500)
+        },500);
     }
 }
 const Circle = function(){
@@ -68,4 +68,4 @@ const Circle = function(){
     stars.push([x,y,rad,colorC])
     ctx.arc(x, y, rad, 0, 2 * Math.PI);
     ctx.fill();
-}
+};
