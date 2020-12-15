416e5baa5cce4fc22158270c99e49a7c08091548
Unknown <skrzynska.konstancja@gmail.com>
2020-12-15 21:56:52 +0100

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
-    cssmin = require('gulp-clean-css');
+    cssmin = require('gulp-clean-css'),
+    jsmin = require('gulp-minify');
 
 gulp.task('style',function(){
     return gulp.src('scss/*.scss')
@@ -11,12 +12,23 @@ gulp.task('style',function(){
       .pipe(cssmin({compatibility: 'ie8'}))
       .pipe(gulp.dest('css'))
       .pipe(browserSync.stream());
-    });
+});
+gulp.task('js',function(){
+    return gulp.src('js/*.js')
+      .pipe(jsmin(({
+        ext: {
+            min: '.min.js'
+        },
+        ignoreFiles: ['-min.js']
+      })))
+      .pipe(gulp.dest('js'));
+});
 gulp.task('serve',gulp.parallel('style',function(){
     browserSync.init({
         server: "."
     });
-    gulp.watch('scss/*.scss', gulp.parallel('style'));
+    // gulp.watch('scss/*.scss', gulp.parallel('style'));
+    gulp.watch('scss/*.scss', gulp.task('style')).on('change', browserSync.reload);
     gulp.watch('*.html').on('change', browserSync.reload);
     gulp.watch('js/*.js').on('change', browserSync.reload);
 }));
